96a7e68b3db18da4f1ca735ae501e5e44f353a9e
Ambar Khare <ambar.khare24@gmail.com>
2021-07-27 08:51:27 -0700

    commit tree 9696c7963261a3b345a68b42dec6321f30b82ffe

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
 
+def sort_array_asc(integers)
+    integers.sort
+end
+
+def sort_array_desc(integers)
+    integers.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(strings)
+    strings.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+    array[index], array[destination_index] = array[destination_index], array[index]
+    array
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(strings)
+    strings.collect {|string| string[2] = "$"}
+    strings
+end
+
+def find_a(strings)
+    strings.select {|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+    integers.inject(:+)
+end
+
+#def add_s(words)
+#words.each_with_index.collect {|word, index| index == 1 ? word : word << "s"}
+#end
+
+def add_s(words)
+    words.each_with_index {|word, index| word << "s" unless index == 1}
+end
